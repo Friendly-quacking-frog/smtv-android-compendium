f11da04d389ff67fa3ce2959161e7fd1302e1f16
a.bulychev <a.bulychev@innopolis.ru>
2021-12-15 17:32:03 +0300

    First major push/commit Done: 1. Pages fo demons 2. Skills 3. Ailments TODO: 1. Search and sort 2. Fusion stats

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> names = <String>['Entry A', 'Entry B', 'Entry C', 'nothing'];
   late Future<List> readFile;
+  late Future<List> skillsList;
 
-  Future<List> loadAsset() async {
+  Future<List> loadDataAsset() async {
     var temp = [];
     var data = await rootBundle.loadString('assets/data.csv');
     temp = data.split('\n');
@@ -68,10 +69,28 @@ class _MyHomePageState extends State<MyHomePage> {
     return temp2;
   }
 
+  Future<List> loadSkillsList() async {
+    var temp = [];
+    var data = await rootBundle.loadString('assets/pages.csv');
+    var temp2 = data.split('\n');
+    for (int i = 0; i < temp2.length; i++) {
+      var tempList = [];
+      var a = temp2[i].split(';');
+      for (int j = 0; j < a.length; j++) {
+        var b = a[j].split(':');
+        tempList.add(b);
+      }
+      temp.add(tempList);
+      print('');
+    }
+    return temp;
+  }
+
   @override
   void initState() {
     super.initState();
-    readFile = loadAsset();
+    readFile = loadDataAsset();
+    skillsList = loadSkillsList();
   }
 
   @override
@@ -82,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: FutureBuilder(
-          future: readFile,
+          future: Future.wait([readFile, skillsList]),
           builder: (
             BuildContext context,
             AsyncSnapshot<List> snapshot,
@@ -90,9 +109,12 @@ class _MyHomePageState extends State<MyHomePage> {
             Widget child;
             if (snapshot.hasData) {
               child = ListView.builder(
-                itemCount: snapshot.data?.length,
+                itemCount: snapshot.data?[0].length,
                 itemBuilder: (BuildContext context, int index) {
-                  return MyListItem(list: snapshot.data![index]);
+                  return MyListItem(
+                    list: snapshot.data![0][index],
+                    skillsList: snapshot.data![1][index],
+                  );
                 },
               );
             } else if (snapshot.hasError) {
@@ -112,160 +134,369 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class MyListItem extends StatelessWidget {
   var list = [];
+  var skillsList = [];
+  List<Widget> weaknessElementList = [];
+  var weaknessIcons = [
+    'assets/icons/phy.png',
+    'assets/icons/fir.png',
+    'assets/icons/ice.png',
+    'assets/icons/ele.png',
+    'assets/icons/force.png',
+    'assets/icons/lig.png',
+    'assets/icons/cur.png',
+  ];
+  List<Widget> affinityElementList = [];
+  var affinityIcons = [
+    'assets/icons/phy.png',
+    'assets/icons/fir.png',
+    'assets/icons/ice.png',
+    'assets/icons/ele.png',
+    'assets/icons/force.png',
+    'assets/icons/lig.png',
+    'assets/icons/cur.png',
+    'assets/icons/alm.png',
+    'assets/icons/ail.png',
+    'assets/icons/rec.png',
+    'assets/icons/sup.png',
+  ];
 
-  MyListItem({Key? key, required this.list}) : super(key: key);
+  MyListItem({Key? key, required this.list, required this.skillsList})
+      : super(key: key);
+
+  Widget weaknessColoredText(String value) {
+    TextStyle style = new TextStyle();
+    switch (value) {
+      case 'rs':
+        style = const TextStyle(color: Colors.teal);
+        break;
+      case 'nu':
+        style = const TextStyle(color: Colors.black);
+        break;
+      case 'wk':
+        style = const TextStyle(color: Colors.redAccent);
+        break;
+      case 'ab':
+        style = const TextStyle(color: Colors.lightGreen);
+        break;
+      case 'rp':
+        style = const TextStyle(color: Colors.lightBlueAccent);
+        break;
+      default:
+        return const Text('');
+    }
+    return Text(value, style: style);
+  }
+
+  void initState() {
+    for (int i = 0; i < weaknessIcons.length; i++) {
+      weaknessElementList.add(Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          width: 26,
+          child: Column(
+            children: [
+              Image.asset(weaknessIcons[i]),
+              weaknessColoredText(list[i + 10])
+            ],
+          )));
+    }
+    for (int i = 0; i < affinityIcons.length; i++) {
+      affinityElementList.add(Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          width: 26,
+          child: Column(
+            children: [Image.asset(affinityIcons[i]), Text(list[i + 17])],
+          )));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    initState();
     return ExpansionTile(
-        title: Row(
-          children: [
-            SizedBox(
-                width: 175,
-                child: Column(
-                  children: [
-                    Container(height: 8),
-                    Row(
-                      children: [
-                        Text(list[0], style: const TextStyle(fontSize: 15)),
-                        Container(width: 20),
-                        Text('LVL:' + list[1],
-                            style: const TextStyle(fontSize: 15))
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text(list[2], style: const TextStyle(fontSize: 18))
-                      ],
-                    )
-                  ],
-                )),
-            Column(
-              children: [
-                Container(height: 8),
-                Row(
-                  children: [Text('HP:' + list[3])],
-                ),
-                Row(
-                  children: [Text('MP:' + list[4])],
-                )
-              ],
-            ),
-            Container()
-          ],
-        ),
+      title: Row(
+        children: [
+          SizedBox(
+              width: 175,
+              child: Column(
+                children: [
+                  Container(height: 8),
+                  Row(
+                    children: [
+                      Text(list[0], style: const TextStyle(fontSize: 15)),
+                      Container(width: 20),
+                      Text('LVL:' + list[1],
+                          style: const TextStyle(fontSize: 15))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(list[2], style: const TextStyle(fontSize: 18))
+                    ],
+                  )
+                ],
+              )),
+          Column(
+            children: [
+              Container(height: 8),
+              Row(
+                children: [Text('HP:' + list[3])],
+              ),
+              Row(
+                children: [Text('MP:' + list[4])],
+              )
+            ],
+          ),
+          Container()
+        ],
+      ),
       children: [
         Row(
           children: [
             //stats info
             Column(
               children: [
-                Text('STR:'+list[5]),
-                Text('VIT:'+list[6]),
-                Text('MAG:'+list[7]),
-                Text('AGI:'+list[8]),
-                Text('LUK:'+list[9]),
+                Text('STR:' + list[5]),
+                Text('VIT:' + list[6]),
+                Text('MAG:' + list[7]),
+                Text('AGI:' + list[8]),
+                Text('LUK:' + list[9]),
               ],
             ),
             Column(
               children: [
                 //weakness info
                 Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black)
-                      ),
-                      width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/phy.png'),
-                            Text(list[10])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/fir.png'),
-                            Text(list[11])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/ice.png'),
-                            Text(list[12])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/ele.png'),
-                            Text(list[13])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/force.png'),
-                            Text(list[14])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/lig.png'),
-                            Text(list[15])
-                          ],
-                        )
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black)
-                        ),
-                        width: 26,
-                        child: Column(
-                          children: [
-                            Image.asset('assets/icons/cur.png'),
-                            Text(list[16])
-                          ],
-                        )
-                    )
-                  ],
+                  children: const [Text('Weaknesses')],
                 ),
+                Row(children: weaknessElementList),
                 //affinity info
-                Row()
+                Row(
+                  children: const [Text('Affinities')],
+                ),
+                Row(children: affinityElementList)
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailedPage(
+                                    list: list,
+                                    skillsList: skillsList,
+                                  )));
+                    },
+                    icon: const Icon(Icons.arrow_forward_ios)),
               ],
             )
           ],
         )
       ],
     );
+  }
+}
+
+class DetailedPage extends StatelessWidget {
+  DetailedPage({Key? key, required this.list, required this.skillsList})
+      : super(key: key);
+
+  List<Widget> weaknessElementList = [];
+  List<Widget> affinityElementList = [];
+  List<Widget> weaknessAilmentList = [];
+  List<List<Widget>> skillInfo = [];
+  var affinityIcons = [
+    'assets/icons/phy.png',
+    'assets/icons/fir.png',
+    'assets/icons/ice.png',
+    'assets/icons/ele.png',
+    'assets/icons/force.png',
+    'assets/icons/lig.png',
+    'assets/icons/cur.png',
+    'assets/icons/alm.png',
+    'assets/icons/ail.png',
+    'assets/icons/rec.png',
+    'assets/icons/sup.png',
+  ];
+  var weaknessIcons = [
+    'assets/icons/phy.png',
+    'assets/icons/fir.png',
+    'assets/icons/ice.png',
+    'assets/icons/ele.png',
+    'assets/icons/force.png',
+    'assets/icons/lig.png',
+    'assets/icons/cur.png',
+  ];
+  var ailmentNames = ['Charm', 'Seal', 'Panic', 'Poison', 'Sleep', 'Mirage'];
+  var list = [];
+  var skillsList = [];
+
+  Widget weaknessColoredText(String value) {
+    TextStyle style = new TextStyle();
+    switch (value) {
+      case 'rs':
+        style = const TextStyle(color: Colors.teal);
+        break;
+      case 'nu':
+        style = const TextStyle(color: Colors.black);
+        break;
+      case 'wk':
+        style = const TextStyle(color: Colors.redAccent);
+        break;
+      case 'ab':
+        style = const TextStyle(color: Colors.lightGreen);
+        break;
+      case 'rp':
+        style = const TextStyle(color: Colors.lightBlueAccent);
+        break;
+      default:
+        return const Text('');
+    }
+    return Text(value, style: style);
+  }
+
+  void initState() {
+    skillsList.removeLast();
+    for (int i = 0; i < weaknessIcons.length; i++) {
+      weaknessElementList.add(Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          width: 26,
+          child: Column(
+            children: [
+              Image.asset(weaknessIcons[i]),
+              weaknessColoredText(list[i + 10])
+            ],
+          )));
+    }
+    for (int i = 0; i < affinityIcons.length; i++) {
+      affinityElementList.add(Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          width: 26,
+          child: Column(
+            children: [Image.asset(affinityIcons[i]), Text(list[i + 17])],
+          )));
+    }
+    for (int i = 0; i < 6; i++) {
+      weaknessAilmentList.add(Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+          child: Column(
+            children: [
+              Text(ailmentNames[i]),
+              weaknessColoredText(list[i + 27])
+            ],
+          )));
+    }
+    for (int i=0; i<skillsList.length; i++){
+      List<Widget> temp = [];
+      temp.add(Image.asset('assets/icons/'+skillsList[i][0]+'.png'));
+      for (int j=1; j<skillsList[i].length; j++){
+        temp.add(Text(skillsList[i][j]));
+        temp.add(Container(width: 2));
+      }
+      temp.removeLast();
+      skillInfo.add(temp);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    initState();
+    return Scaffold(
+        appBar: AppBar(title: const Text('Detailed Data')),
+        body: Center(
+            child: Column(
+              children: [
+                Container(height: 30),
+                Text(list[2], style: const TextStyle(fontSize: 35)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Race: ' + list[0]),
+                    Container(width: 10),
+                    Text('LVL: ' + list[1]),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('HP: '+list[3]),
+                    Container(width: 20),
+                    Text('MP: '+list[4])
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('STR:' + list[5]),
+                    Container(width: 5),
+                    Text('VIT:' + list[6]),
+                    Container(width: 5),
+                    Text('MAG:' + list[7])
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('AGI:' + list[8]),
+                    Container(width: 5),
+                    Text('LUK:' + list[9])
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('Element weakness', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: weaknessElementList,
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('Skill affinity', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: affinityElementList,
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('Ailment weakness', style: TextStyle(fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: weaknessAilmentList,
+                    )
+                  ],
+                ),
+                Center(
+                  child: ListView.builder(
+                    itemCount: skillsList.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index){
+                      return ExpansionTile(
+                          title: Row(
+                            children: [
+                              skillInfo[index][0],
+                              skillInfo[index][1],
+                              skillInfo[index][2],
+                              skillInfo[index][3]
+                            ],
+                          ),
+                          children: [
+                            skillInfo[index][5],
+                            skillInfo[index][6],
+                            skillInfo[index][7],
+                          ],
+
+                      );
+                    }
+                ))
+
+
+          ],
+        )));
   }
 }
